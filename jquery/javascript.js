45b790f8961e4d367c375ede5f9c382ae4450ece
SUJOY ROY <88615084+suzoyroy@users.noreply.github.com>
2022-02-08 14:04:50 +0600

    Add files via upload

---
+++ jquery/javascript.js
@@ -0,0 +1,13 @@
+var cross = document.getElementById("cross");
+var menu = document.getElementById("menu");
+var dropdown = document.getElementById("dropdown");
+
+
+
+function outside() {
+    dropdown.style.top = "-600px";
+}
+
+function inside() {
+    dropdown.style.top = "0px";
+}
